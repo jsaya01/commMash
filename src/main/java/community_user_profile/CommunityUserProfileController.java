1de58510fdb1b5834e438871921e65ae47bba121
Logan <lnlawson@calpoly.edu>
2019-05-06 14:52:30 -0700

    Update CommunityUserProfileController.java

--- src/main/java/community_user_profile/CommunityUserProfileController.java
+++ src/main/java/community_user_profile/CommunityUserProfileController.java
@@ -2,8 +2,6 @@ package community_user_profile;
 
 import community.Community;
 import community.CommunityRepository;
-import community_tags.CommunityTags;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
