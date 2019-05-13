828fd1316d8eed2f702bc1a1f8c1217830d949b4
Logan <lnlawson@calpoly.edu>
2019-05-13 10:53:10 -0700

    communityuserprofile update

--- src/main/java/community_user_profile/CommunityUserProfileController.java
+++ src/main/java/community_user_profile/CommunityUserProfileController.java
@@ -46,8 +46,8 @@ public class CommunityUserProfileController {
     
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity getCommunityUserProfile(@RequestParam long upid) {
-        Future<CommunityUserProfile> communityUserProfile = communityUserProfileRepository.findCommunityUserProfile(upid);
+    public ResponseEntity getCommunityUserProfile(@RequestParam long uid, @RequestParam long cid) {
+        Future<CommunityUserProfile> communityUserProfile = communityUserProfileRepository.findCommunityUserProfile(uid, cid);
 
         try{
             communityUserProfile.get().getDescription();
@@ -74,11 +74,11 @@ public class CommunityUserProfileController {
     
     @RequestMapping(method = RequestMethod.DELETE)
     public ResponseEntity delete(@RequestBody CommunityUserProfile communityUserProfile) {
-    	ResponseEntity res = getCommunityUserProfile(communityUserProfile.getUpid());
+    	ResponseEntity res = getCommunityUserProfile(communityUserProfile.getUid(), communityUserProfile.getCid());
     	
     	if(res.getStatusCode() == HttpStatus.OK) {
     		CommunityUserProfile temp = (CommunityUserProfile) res.getBody();
-    		communityUserProfileRepository.deleteById(temp.getUpid());
+    		communityUserProfileRepository.delete(temp);
             return ResponseEntity.status(HttpStatus.OK).body("Deleted");
     	}
     	
