599ba96f89b0804928d019edb01de7993c297281
Logan <lnlawson@calpoly.edu>
2019-05-27 13:48:44 -0700

    CUP dual key implemented

--- src/main/java/cm/community_user_profile/CommunityUserProfile.java
+++ src/main/java/cm/community_user_profile/CommunityUserProfile.java
@@ -1,14 +1,20 @@
 package cm.community_user_profile;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 
 @Entity
-public class CommunityUserProfile {
+@IdClass(CUP_CompKeys.class)
+public class CommunityUserProfile{
 
     @Id
 	private long uid;
+    @Id
 	private long cid;
+    
 	private String description;
 	
 	public CommunityUserProfile() {
