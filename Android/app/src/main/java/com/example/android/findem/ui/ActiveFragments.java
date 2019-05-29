f29fbafc22e7338e3c6a74b86660084ef9b0b63d
jsaya01 <jsaya01@gmail.com>
2019-05-29 11:44:50 -0700

    active fragments code smell fix

--- Android/app/src/main/java/com/example/android/findem/ui/ActiveFragments.java
+++ Android/app/src/main/java/com/example/android/findem/ui/ActiveFragments.java
@@ -1,13 +1,18 @@
 package com.example.android.findem.ui;
 
 public class ActiveFragments {
+
+    private ActiveFragments() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static String tagHomeFragment = "tag-home-fragment";
     public static String tagMessagesFragment = "tag-messages-fragment";
     public static String tagSettingsFragment = "tag-settings-fragment";
-    public static String tagSearchCommFragment = "tag-settings-fragment";
-    public static String tagCreateCommFragment = "tag-settings-fragment";
-    public static String tagEditProfileFragment = "tag-settings-fragment";
-    public static String tagCommunityFragment = "tag-settings-fragment";
-    public static String tagFilterResultsFragment = "tag-settings-fragment";
-    public static String tagMatchingFragment = "tag-settings-fragment";
+    public static String tagSearchCommFragment = "tag-search-fragment";
+    public static String tagCreateCommFragment = "tag-create-fragment";
+    public static String tagEditProfileFragment = "tag-edit-fragment";
+    public static String tagCommunityFragment = "tag-community-fragment";
+    public static String tagFilterResultsFragment = "tag-filter-fragment";
+    public static String tagMatchingFragment = "tag-matching-fragment";
 }
