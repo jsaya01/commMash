5ecaa9e0cab97e68012d82067c6e6d3b73e5fdc2
nabishop <35745821+nabishop@users.noreply.github.com>
2019-05-31 11:31:48 -0700

    made active fragments strings final

--- Android/app/src/main/java/com/example/android/findem/ui/ActiveFragments.java
+++ Android/app/src/main/java/com/example/android/findem/ui/ActiveFragments.java
@@ -6,13 +6,13 @@ public class ActiveFragments {
         throw new IllegalStateException("Utility class");
     }
 
-    public static String tagHomeFragment = "tag-home-fragment";
-    public static String tagMessagesFragment = "tag-messages-fragment";
-    public static String tagSettingsFragment = "tag-settings-fragment";
-    public static String tagSearchCommFragment = "tag-search-fragment";
-    public static String tagCreateCommFragment = "tag-create-fragment";
-    public static String tagEditProfileFragment = "tag-edit-fragment";
-    public static String tagCommunityFragment = "tag-community-fragment";
-    public static String tagFilterResultsFragment = "tag-filter-fragment";
-    public static String tagMatchingFragment = "tag-matching-fragment";
+    public static final String tagHomeFragment = "tag-home-fragment";
+    public static final String tagMessagesFragment = "tag-messages-fragment";
+    public static final String tagSettingsFragment = "tag-settings-fragment";
+    public static final String tagSearchCommFragment = "tag-search-fragment";
+    public static final String tagCreateCommFragment = "tag-create-fragment";
+    public static final String tagEditProfileFragment = "tag-edit-fragment";
+    public static final String tagCommunityFragment = "tag-community-fragment";
+    public static final String tagFilterResultsFragment = "tag-filter-fragment";
+    public static final String tagMatchingFragment = "tag-matching-fragment";
 }
