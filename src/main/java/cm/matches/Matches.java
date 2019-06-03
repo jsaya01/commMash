201c57c0d2e846db16669731b230353ed3a18220
jsaya01 <jsaya01@gmail.com>
2019-06-03 10:58:17 -0700

    matches fixed

--- src/main/java/cm/matches/Matches.java
+++ src/main/java/cm/matches/Matches.java
@@ -1,6 +1,10 @@
 package cm.matches;
 
+import java.io.DataOutputStream;
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,13 +24,16 @@ public class Matches {
     private long uid2;
     
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    private Timestamp tstamp;
+    private Date tstamp;
     
-    public Matches(long uid1, long uid2, Timestamp tstamp) {
+    public Matches(long uid1, long uid2) throws ParseException {
     	
     	this.uid1 = uid1;
     	this.uid2 = uid2;
-    	this.tstamp = tstamp;
+		Date date = new Date();
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		java.util.Date temp = sdf1.parse(sdf1.format(date));
+    	this.tstamp = temp;
     }
 
 	public long getMid() {
@@ -41,7 +48,7 @@ public class Matches {
 		return uid2;
 	}
 
-	public Timestamp getTstamp() {
+	public Date getTstamp() {
 		return tstamp;
 	}
 
