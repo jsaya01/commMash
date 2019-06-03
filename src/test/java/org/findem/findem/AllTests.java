20d3fb42100b108ee89dbdcc69798c469d0207af
Logan <lnlawson@calpoly.edu>
2019-06-03 10:07:23 -0700

    test suite update

--- src/test/java/org/findem/findem/AllTests.java
+++ src/test/java/org/findem/findem/AllTests.java
@@ -1,5 +1,7 @@
 package org.findem.findem;
 
+import org.findem.findem.lawson_test.LawsonTest;
+import org.findem.findem.lawson_test.LawsonTest2;
 import org.findem.findem.saya_test.SayaTest;
 import org.findem.findem.saya_test.SayaTest2;
 import org.junit.runner.RunWith;
@@ -7,7 +9,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SayaTest.class, SayaTest2.class })
+@SuiteClasses({ SayaTest.class, SayaTest2.class, LawsonTest.class, LawsonTest2.class })
 public class AllTests {
 
 }
--- src/test/java/org/findem/findem/lawson_test/LawsonTest.java
+++ src/test/java/org/findem/findem/lawson_test/LawsonTest.java
@@ -3,54 +3,28 @@ package org.findem.findem.lawson_test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.*;
 
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 import cm.Application;
-import cm.community.Community;
 import cm.community.CommunityController;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CommunityController.class)
 @ContextConfiguration(classes=Application.class)
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-
 public class LawsonTest {
 	
-//	@LocalServerPort
-//	private int port;
-//	
-//	@Autowired
-//	private TestRestTemplate restTemplate;
-//	
-//	@Test
-//	public void CommunityGet() throws Exception {
-//		Community comm = new Community("Racing Club", "path", "I love AI");
-//		assertThat(restTemplate.getForEntity("http://localhost:" + port + "/community?name=AI Club", Community.class)).
-//			contains()
-//		
-//	}
 	
 	@Autowired
 	private MockMvc mvc;
